533a27a425d09608acefcf53fc0334a75a7fe71e
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-04-14 08:03:40 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,14 +33,38 @@ export default {
     // spacing (Tailwind might strip dynamic ones)
     "p-2","p-4","p-6",
     "m-2","m-4","m-6",
+
+     "from-purple-600",
+    "to-pink-600",
+    "from-green-500",
+    "to-green-600",
+    "from-blue-600",
+    "to-cyan-600",
+    "from-red-600",
+    "to-orange-600",
   ],
-  theme: {
+   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
         border: "var(--border)",
         ring: "var(--ring)",
+
+        primary: "var(--primary)",
+        "primary-foreground": "var(--primary-foreground)",
+
+        secondary: "var(--secondary)",
+        "secondary-foreground": "var(--secondary-foreground)",
+
+        muted: "var(--muted)",
+        "muted-foreground": "var(--muted-foreground)",
+
+        accent: "var(--accent)",
+        "accent-foreground": "var(--accent-foreground)",
+
+        destructive: "var(--destructive)",
+        "destructive-foreground": "var(--destructive-foreground)",
       },
     },
   },
