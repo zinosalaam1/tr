2c287b2c7461055d3ac11cb962e57395273aa875
zinosalaam1 <bolatitosalaam7@gmail.com>
2026-04-14 07:50:04 +0100

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        border: "var(--border)",
+        ring: "var(--ring)",
+      },
+    },
+  },
+  safelist: [
+  "bg-background",
+  "text-foreground",
+  "border-border",
+  "outline-ring",
+],
+  plugins: [],
+}
